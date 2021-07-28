a9c81cd0a9f011cb274b3e997ebb8ee1f798907a
unknown <sabbirhs@yahoo.com>
2021-07-28 11:54:19 +0600

    yesterdays update

--- src/comp/data/data.js
+++ src/comp/data/data.js
@@ -1,5 +1,6 @@
 
 import DeckGL from "deck.gl";
+import {MapView, FirstPersonView} from '@deck.gl/core';
 import { StaticMap } from "react-map-gl";
 import React, { Component } from "react";
 import axios from "axios";
@@ -12,6 +13,9 @@ class Data extends Component {
     super();
     this.state = {
       datas: [],
+      selectOptions : [],
+      id: "",
+      name: ''
     };
   }
   componentDidMount() {
@@ -46,24 +50,33 @@ class Data extends Component {
       latitude: 23,
       longitude: 90,
       zoom: 8,
-      bearing: 78,
+      bearing: 68,
       pitch: 55
     }; 
 
-    return(<React.Fragment>
+    return(
+    
+    <React.Fragment>
+      
+      <div style={{ height: '10%' }}>
       <DeckGL
        initialViewState={INITIAL_VIEW_STATE}
-       height="90%"
-       width="100%"
+       height="98%"
+       width="98%"
        controller={true} 
        layers={RenderLayers({ data: datas})} 
       >
+        <MapView id="map" width="100%" controller={true}>
         <StaticMap
           mapStyle="mapbox://styles/mapbox/streets-v11"
           mapboxApiAccessToken="pk.eyJ1Ijoic2FiYmlyIiwiYSI6ImNpenV0b3d0ZTAwdjAyd3IycjU4aTc2NjkifQ.HF6YOQbCT3Eqy2ohOL8BDg"
           
          />
+         </MapView>
+         <FirstPersonView width="80%" x="100%" y="100%" fovy={10} />
       </DeckGL>
+      </div>
+      
       
      </React.Fragment>
      ) 
--- src/comp/deck-gl-l.jsx
+++ src/comp/deck-gl-l.jsx
@@ -1,36 +1,48 @@
 import { ColumnLayer } from "deck.gl";
 import { scaleLinear } from "d3-scale";
-import de from "../datas/B10721.json";
+import de from "../datas/daily.json";
 export const RenderLayers = (props) => {
     let maxActive, minActive;
-    const radiusColumns = 55000;
+    const radiusColumns = 4000;
     const { data} = props;
     
     
     const dat = de.map(function (location) {
         return {
           active: location.cases,
+          death: location.death,
           district: location.district,
           coordinates: [location.lon, location.lat]
         };
       });
     console.log(dat);  
-    maxActive = Math.max(200);
-    minActive = Math.min(5);
-    const elevation = scaleLinear([minActive, maxActive], [0, 900]);
+    maxActive = Math.max(6000);
+    minActive = Math.min(1);
+    const elevation = scaleLinear([minActive, maxActive], [0, 4000]);
     return [
-    
     new ColumnLayer({
-        id: "deaths",
+        id: "cases",
         data:dat,
         pickable: true,
         extruded: true,
         getPosition: d => d.coordinates,
         diskResolution: 20,
-        radius: 4000,
-        elevationScale: 1.5,
-        getFillColor: d =>[5, 108, d.active * 0.5, 235],
+        radius: radiusColumns,
+        elevationScale: 80,
+        getFillColor: d =>[12, 12, 205, 235],
         getElevation: d => elevation(d.active),
         }),
+    new ColumnLayer({
+          id: "death",
+          data:dat,
+          pickable: true,
+          extruded: true,
+          getPosition: d => d.coordinates,
+          diskResolution: 20,
+          radius: radiusColumns,
+          elevationScale: 1200,
+          getFillColor: d =>[250, 108, 11, 235],
+          getElevation: d => elevation(d.death),
+          }),    
  ];
 }
